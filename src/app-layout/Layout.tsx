cc3d1a848fc57309f75d3c8ef4db33e1d3c23fae
Holt Mansfield <holtmansfield@gmail.com>
2023-03-04 10:40:59 -0500

    Weird hack because the media query is wrong the first time

--- src/app-layout/Layout.tsx
+++ src/app-layout/Layout.tsx
@@ -3,7 +3,7 @@ import { AppBar } from '@/app-layout/app-bar/AppBar'
 import { SideMenu } from '@/app-layout/side-menu/SideMenu'
 import { SideMenuContent } from '@/app-layout/side-menu/SideMenuContent'
 import { MobileAppBar } from '@/app-layout/app-bar/MobileAppBar'
-import { ReactElement } from 'react'
+import { ReactElement, useEffect, useState } from 'react'
 
 interface LayoutProps {
   children: ReactElement
@@ -11,9 +11,15 @@ interface LayoutProps {
 
 export const Layout = ({ children }: LayoutProps) => {
   const theme = useTheme()
+  // This is wrong the first time (toDo)
   const isDevice = useMediaQuery(theme.breakpoints.down('sm'))
+  const [hasRendered, setHasRendered] = useState(false)
 
-  console.log(theme.breakpoints.down('sm'))
+  useEffect(() => {
+    setHasRendered(true)
+  },[])
+
+  if (!hasRendered) return null
 
   if (isDevice) {
     return (
